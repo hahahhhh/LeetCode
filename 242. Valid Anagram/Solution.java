e4bd33c42a1a284de8ade91b3156e5ce6c99fe58
hahahhhh <36784549+hahahhhh@users.noreply.github.com>
2018-06-29 16:39:18 +0800

    Valid Anagram
    Optimized

--- 242. Valid Anagram/Solution.java
+++ 242. Valid Anagram/Solution.java
@@ -1,9 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       char[] a =s.toCharArray();
-        char[] b =t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b); 
+       char[] count = new char[26];
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            count[c-'a']--;
+        }
+        for(int i:count){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
