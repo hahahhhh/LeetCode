ca1413d603caeb1f5e3d3641f514f784bd3983e5
hahahhhh <pkq_q@163.com>
2020-07-20 12:44:36 +0800

    168.Excel Sheet Column Title 1 Solutions

---
+++ 168. Excel Sheet Column Title/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while(n >0){
+            int c = n %26;
+            if(c==0){
+                c = 26;
+                n -= 1;
+            }
+            stringBuilder.insert(0,(char)('A'+c-1));
+            n /= 26;
+        }
+        return stringBuilder.toString();
+    }
+
+}
